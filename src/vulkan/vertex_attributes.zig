af8d53efeef75482bfa8b29cf7e7c7bed9da54bf
Piotr Doan <doanpiotr@gmail.com>
2023-07-20 22:16:54 +0200

    vulkan: add float2 vertex attribute

--- src/vulkan/vertex_attributes.zig
+++ src/vulkan/vertex_attributes.zig
@@ -1,12 +1,14 @@
 const c = @import("../c.zig");
 
 pub const VertexAttribute = enum {
+    Float2,
     Float3,
     Float4,
 };
 
 pub fn getVertexAttributeSize(attribute: VertexAttribute) u32 {
     switch (attribute) {
+        .Float2 => return @sizeOf([2]f32),
         .Float3 => return @sizeOf([3]f32),
         .Float4 => return @sizeOf([4]f32),
     }
@@ -14,6 +16,7 @@ pub fn getVertexAttributeSize(attribute: VertexAttribute) u32 {
 
 pub fn getVertexAttributeFormat(attribute: VertexAttribute) c.VkFormat {
     switch (attribute) {
+        .Float2 => return c.VK_FORMAT_R32G32_SFLOAT,
         .Float3 => return c.VK_FORMAT_R32G32B32_SFLOAT,
         .Float4 => return c.VK_FORMAT_R32G32B32A32_SFLOAT,
     }
